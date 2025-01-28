451c0c3ffba22cb6f1353a771891c7f733df7b71
Nathan Lufuluabo <lufuluabon@outlook.fr>
2025-01-28 14:43:59 +0100

    Added logs

--- src/components/SoundDetailScreen.js
+++ src/components/SoundDetailScreen.js
@@ -4,7 +4,7 @@ import { View, FlatList, Text, StyleSheet, TouchableOpacity } from 'react-native
 
 export default function SoundDetailScreen({route}) {
     const { item } = route.params.item;
-
+    
     const playMidi = async (midiJson) => {
         // Convert MIDI JSON to audio file (this step requires a server-side conversion or a different approach)
         // For simplicity, let's assume you have a URL to an audio file
@@ -15,16 +15,14 @@ export default function SoundDetailScreen({route}) {
     };
 
     const renderTrack = function({ item, index }) {
-        console.log(item?.notes?.[0]);
-        
         const notes = item?.notes?.map(note => {
             return {
                 name: note?.name,
                 time: note?.time,
             };
         });
-        console.log(notes);
-
+        // console.log(notes);
+        
         return (
             <View style={styles.trackRow}>
                 {/* <Text style={styles.trackText}>Track {index + 1}: {Object.keys(item.instrument.name[0]).map(i => (i + ";"))}</Text> */}
@@ -42,7 +40,20 @@ export default function SoundDetailScreen({route}) {
                 keyExtractor={(track, index) => index.toString()}
                 contentContainerStyle={styles.trackList}
             />
-            <Text style={styles.trackText}>Total tracks: {item.content.tracks.length}</Text>
+            <Text style={styles.trackText}>Tempo (bpm): {item.content.header.tempos[0].bpm} à {item.content.header.tempos[0].ticks} ticks</Text>
+            <Text style={styles.trackText}>Tempo (bpm): {item.content.header.tempos[1].bpm} à {item.content.header.tempos[1].ticks} ticks</Text>
+            <Text style={styles.trackText}>Tempo (bpm): {item.content.header.tempos[2].bpm} à {item.content.header.tempos[2].ticks} ticks</Text>
+            <Text style={styles.trackText}>PPQ : {item.content.header.ppq}</Text>
+            <Text style={styles.trackText}>EoTT : {item.content.tracks[0].endOfTrackTicks}</Text>
+            <Text style={styles.trackText}>EoTT : {item.content.tracks[1].endOfTrackTicks}</Text>
+            <Text style={styles.trackText}>EoTT : {item.content.tracks[2].endOfTrackTicks}</Text>
+            <Text style={styles.trackText}>EoTT : {item.content.tracks[3].endOfTrackTicks}</Text>
+            <Text style={styles.trackText}>EoTT : {item.content.tracks[4].endOfTrackTicks}</Text>
+            <Text style={styles.trackText}>EoTT : {item.content.tracks[5].endOfTrackTicks}</Text>
+            <Text style={styles.trackText}>EoTT : {item.content.tracks[6].endOfTrackTicks}</Text>
+            <Text style={styles.trackText}>EoTT : {item.content.tracks[7].endOfTrackTicks}</Text>
+            <Text style={styles.trackText}>EoTT : {item.content.tracks[8].endOfTrackTicks}</Text>
+            {/* <Text style={styles.trackText}>PPQ : {Object.keys(item.content.tracks[0].endOfTrackTicks).map(i => (i + ";"))}</Text> */}
             <TouchableOpacity style={styles.playButton} onPress={() => playMidi(item.content)}>
                 <Text style={styles.playButtonText}>Play</Text>
             </TouchableOpacity>
